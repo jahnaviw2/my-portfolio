7d9480535361c8f10540311ec6d8bf58dee5134d
jahnaviw2 <jahnaviw2@gmail.com>
2026-08-13 14:01:23 +0530

    added config changes to nect.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/my-portfolio',
+  assetPrefix: '/my-portfolio/',
   images: {
     unoptimized: true, // Required for static exports (e.g., GitHub Pages)
   },
 };
 
 export default nextConfig;
+
+
